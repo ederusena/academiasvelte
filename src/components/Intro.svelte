8669a8f362030b93b9ae91bcd8e32ebb9d0bb005
Eder Sena <eder.sena@live.com>
2023-02-23 23:04:14 -0300

    criando component section intro

---
+++ src/components/Intro.svelte
@@ -0,0 +1,62 @@
+<script>
+	import Button from '@components/Button.svelte';
+    import { useNavigate,useLocation } from 'svelte-navigator';
+    
+    const navigate = useNavigate();
+    const location = useLocation()
+
+	export let title;
+	export let content;
+	export let btn;
+	export let href;
+	export let img;
+	export let icon;
+
+	function openSMitem(path) {
+		closeSnav();
+		console.log(path);
+		setTimeout(() => {
+			navigate(path, {
+				state: { from: $location.pathname },
+				replace: true
+			});
+		}, 300);
+	}
+</script>
+
+<section class="intro" style="background-image: url({img});">
+	<div class="wrap">
+		<h1 class="px-6 text-4xl mb-4 leading-2">
+			{title}<img class="muscle" src={icon} alt="muscle alt" />
+		</h1>
+		<div class="px-6 w-[390px]">
+			<p class="mb-4">
+				{content}
+			</p>
+		</div>
+		<Button class="btn primary sm mx-6">Modalidades</Button>
+	</div>
+</section>
+
+<style lang="postcss">
+	.intro {
+		@apply min-h-[360px]
+			relative
+			bg-cover
+			bg-center
+			grid
+			content-center
+			justify-start
+			after:absolute
+			after:inset-0
+			after:bg-slate-900/50;
+	}
+
+	.wrap {
+		@apply z-10 mx-auto;
+	}
+
+	.muscle {
+		@apply inline max-w-[30px] mb-3;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,13 @@
 <script>
-	import imgHome from '@assets/musculacao.jpg';
+	import Intro from '@components/Intro.svelte';
 	import icon from '@assets/icon-muscle.png';
-	import Button from './Button.svelte';
+	import imgHome from '@assets/musculacao.jpg';
+
 </script>
 
-<section class="intro">
-	<div class="wrap">
-		<h1 class="px-6 text-4xl mb-4 leading-2">
-			Voce está pronto<br />para o desafio?<img class="muscle" src={icon} alt="muscle alt" />
-		</h1>
-		<div class="px-6 w-[390px]">
-			<p class="mb-4">
-				A academia local está pronta para te ajudar a chegar mais longe e alcançar sua melhor
-				performance.
-			</p>
-		</div>
-		<Button class="btn primary sm mx-6">Modalidades</Button>
-	</div>
-</section>
-
-<style lang="postcss">
-	.intro {
-		@apply min-h-[360px]
-			bg-[url('../assets/musculacao.jpg')]
-			relative
-			bg-cover
-			bg-center
-			grid
-			content-center
-			justify-start
-			after:absolute
-			after:inset-0
-			after:bg-slate-900/50;
-	}
-
-	.wrap {
-		@apply z-10 mx-auto;
-	}
-
-	.muscle {
-		@apply inline max-w-[30px] mb-3;
-	}
-</style>
+<Intro 
+	title="Musculação e Treinamento Funcional"
+	content="Aqui você encontra uma equipe de profissionais qualificados para te ajudar a alcançar seus objetivos."
+	icon={icon}
+	img={imgHome}
+/>
